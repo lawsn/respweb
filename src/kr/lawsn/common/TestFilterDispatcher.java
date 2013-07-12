3c099bc08ad75816fad6c0e5bf595e66479a966b
lawsn <lawsnland@gmail.com>
2013-07-12 17:30:03 +0900

    반응형웹

---
+++ src/kr/lawsn/common/TestFilterDispatcher.java
@@ -0,0 +1,29 @@
+package kr.lawsn.common;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.struts2.dispatcher.FilterDispatcher;
+
+public class TestFilterDispatcher extends FilterDispatcher {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+		System.out.println("##### TestFilterDispatcher INIT #####");
+		super.init(filterConfig);
+	}
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		
+		System.out.println("##### TestFilterDispatcher doFilter #####");
+		super.doFilter(req, res, chain);
+	}
+
+}
